b739d2a140202a5b7c92eab33f73ddd8381ab4e0
hongsi0905 <blancnours@gmail.com>
2022-10-21 15:47:06 +0900

    221021

---
+++ 221021/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _221021
+{
+    internal class Program
+    {
+        // delegate(대리자) : 함수를 담을 수 있는 자료형(형식)
+
+        delegate void CallEvent();              // 반환형이 없고 매개변수가 없는 함수
+        delegate int MethEvent(int a, int b);    // int반환 2개의 int 매개변수 받는 함수
+        delegate int CalculateEvent(int a, int b);   
+
+        static void Main(string[] args)
+        {
+            Sort sort = new Sort();
+            sort.Start();
+            return;
+
+            CallEvent onCall = Temp;                        // onCall 델리게이트에 Temp함수 대입
+            // onCall() = Sum                               // 다른 형태의 함수는 대입불가
+            onCall();                                       // 대리자통해 함수호출
+
+            MethEvent onMeth = Sum;                         // 형식맞는 Sum대입
+            Console.WriteLine($"Sum : {onMeth(5,2)}");      // 대리자통해 함수호출
+
+            onMeth = Minus;                                 // 형식맞는 Minus대입
+            Console.WriteLine($"Minus : {onMeth(5, 2)}");   // 대리자통해 함수호출
+
+            onMeth = Multiple;                              
+            Console.WriteLine($"Mtpl : {onMeth(5, 2)}");
+
+            Calculate(20, 40, Sum);
+            Calculate(20, 20, Multiple);
+        }
+
+        static void Calculate(int a, int b, CalculateEvent onEvent)
+        {
+            int value = onEvent(a, b);
+            Console.WriteLine($"계산 결과 : {value}");
+        }
+
+        static void Temp()
+        {
+            Console.WriteLine("함ㅅ 1");
+        }
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+        static int Multiple(int a, int b)
+        {
+            return a * a + b * b;
+        }
+
+    }
+}
---
+++ 221021/Sort.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _221021
+{
+    // 일반화 델리게이트
+    delegate int Compare<T>(T a, T b);
+
+    internal class Sort
+    {
+        public void Start()
+        {
+            int[] array = { 5, 1, 3, 2, 7, 9, 10, 0, 6 };
+            Console.WriteLine($"정렬 전 : {string.Join(',', array)}");
+            Console.Write("오름차순 ");
+            BubbleSort(array, Compare);
+            Console.WriteLine($"정렬 후 : {string.Join(',', array)}");
+            Console.WriteLine();
+
+            string[] array2 = { "EE", "AA", "DD", "CC", "BB" };
+            Console.WriteLine($"정렬 전 : {string.Join(',', array2)}");
+            Console.Write("내림차순 ");
+            BubbleSort(array2, Compare, false);
+            Console.WriteLine($"정렬 후 : {string.Join(',', array2)}");
+
+            // 
+            List<string> list = new List<string>();
+            list.Add("DD");
+            list.Add("AA");
+            list.Add("KK");
+
+            list.Sort(Compare);
+
+            Console.WriteLine(string.Join(',', list));
+
+        }
+        void BubbleSort<T>(T[] array,Compare<T> compare, bool isAscending = true)
+        {
+            int com = isAscending ? -1 : 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (compare(array[i], array[j]) == com)
+                        Swap(array, i, j);
+                }
+
+            }
+            void Swap<T>(T[] array, int a, int b)
+            {
+                T temp = array[a];
+                array[a] = array[b];
+                array[b] = temp;
+            }
+        }
+
+        int Compare(int a, int b)
+        {
+            if (a > b)
+                return -1;
+            else if (a < b)
+                return 1;
+            else
+                return 0;
+        }
+        int Compare(string a, string b)
+        {
+            int aNum = 0;
+            int bNum = 0;
+
+            foreach (char c in a)
+                aNum += c;
+
+            foreach (char c in b)
+                bNum += c;
+            if (aNum > bNum)
+                return -1;
+            else if (aNum < bNum)
+                return 1;
+            else
+                return 0;
+
+        }
+    }
+}
